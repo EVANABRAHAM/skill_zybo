a05d209dc7a7876c101525384e3eff6b38c3887e
Evan <evantabraham842@gmail.com>
2026-01-06 21:54:43 +0530

    logout functionality

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,17 @@
+'use client';
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/components/icons/Logo";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    router.push("/login");
+  };
+
   return (
     <header className="w-full h-[70px] bg-[#191919] flex items-center justify-between px-[60px] py-[8px]">
       {/* Logo */}
@@ -16,22 +25,25 @@ export default function Header() {
         <Image
           src="/images/UserCircle.svg"
           alt="User"
-          width={95}
-          height={24}
+          width={30}
+          height={5}
           className="opacity-100"
         />
 
         {/* Logout Text */}
         <button
+          onClick={handleLogout}
           className="
             text-white
             font-inter
             font-semibold
-            text-[15px]
+            text-[12px]
             leading-[19.16px]
             tracking-[-0.03em]
             text-center
             opacity-100
+            hover:opacity-80
+            transition-opacity
           "
         >
           Log Out
